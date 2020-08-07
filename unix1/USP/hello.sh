e3cc242b3730bcae3a27d8578d1b7cff282132a8
Georges Bou Ghantous <George@localhost.localdomain>
2020-08-07 11:37:34 +1000

    unix 1

---
+++ unix1/USP/hello.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#read pattern
+echo -n "enter your name: "
+read name
+echo "Welcome $name"
+echo "Your  mark is $1"
+echo "Your grade is $2"
+echo "Running: $0"
---
+++ unix1/USP/welcome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+user=`whoami`
+echo "Welcome to USP"
+echo -n " My name is $user"
+echo " I am your tutor for Spr2020"
+ls -l
