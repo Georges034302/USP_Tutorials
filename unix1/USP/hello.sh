7ca5bdf0081720b745b8c8e56be27d17e20ba033
Georges Bou Ghantous <George@localhost.localdomain>
2020-08-07 11:46:59 +1000

    reset

--- unix1/USP/hello.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#read pattern
-echo -n "enter your name: "
-read name
-echo "Welcome $name"
-echo "Your  mark is $1"
-echo "Your grade is $2"
-echo "Running: $0"
--- unix1/USP/welcome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-user=`whoami`
-echo "Welcome to USP"
-echo -n " My name is $user"
-echo " I am your tutor for Spr2020"
-ls -l
