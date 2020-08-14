a9aff00d2aad5f8ef37c40eb7205f4b5de5b6f7f
Georges Bou Ghantous <George@localhost.localdomain>
2020-08-14 13:26:38 +1000

    unix2

--- unix2/vowel.sh
+++ unix2/vowel.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#Calculate the number of lowercase vowels from user-input until .
+#Count the lowercase vowels from user input until .
 
 echo -n "Character: "
 read c
-count=0
 
 while [ $c != "." ]
 do
@@ -12,7 +11,8 @@ do
 	then
 		((count++))
 	fi
-	echo -n "Character: "
+	echo -n "Charcater: "
 	read c
 done
+
 echo "Vowel count = $count"
