d6801e9cc25cebefae48452ec66ea6766aa37e03
Georges034302 <georges.boughantous@gmail.com>
2022-01-24 15:08:30 +1100

    reset

--- unix1/grade.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Read your mark from arguments
-#Print the mark on the screen
-
-echo "Your mark is $1"
-
-echo "Running: $0"
--- unix1/welcome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#This is our first script
-
-echo "My name is $(whoami)"
-
-ls -l
-
-touch f4 f5
-
