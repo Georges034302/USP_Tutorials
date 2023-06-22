93d26832fcc9a7bdbcd751a53db0a01e4d0180aa
Georges034302 <georges.boughantous@gmail.com>
2023-06-22 20:05:13 +1000

    reset

--- python0102/welcome.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-# Read a name from STDIN and compare it to Tom or print goodbye
-
-name = input("Name: ")
-
-if name.lower() == "Tom".lower():
-    print("Welcome "+name)
-else:
-    print("Goodbye!")
--- unix3/filehandler.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# use IFS to read file content
-
-while IFS= read -r line;do
-	echo $line
-done < "$1"
